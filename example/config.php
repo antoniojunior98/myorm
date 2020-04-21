d92a97a796c5f06ae25ca5eb341beded7d5a410c
myorm <antoniojuniorti98@gmail.com>
2020-04-20 21:21:22 -0300

    commit inicial

---
+++ example/config.php
@@ -0,0 +1,13 @@
+<?php
+define("ENVIRONMENT", "development");
+
+define("DB_NAME", "DB_devmazon");
+
+define("HOST_DEVELOPMENT", "localhost");
+define("DB_USER_DEVELOPMENT", "root");
+define("DB_PASS_DEVELOPMENT", "");
+
+define("HOST_PRODUCTION", "");
+define("DB_USER_PRODUCTION", "");
+define("DB_PASS_PRODUCTION", "");
+?>
